53bb37ea7161d6bda9de8fca2b160d16ae665335
Mithun Ayachit <m0t0rbr3th@gmail.com>
2009-07-13 01:24:54 -0500

    20090712

--- Net/Rapidshare.pm
+++ Net/Rapidshare.pm
@@ -2,11 +2,10 @@ package Net::Rapidshare;
 
 use strict;
 use warnings;
-use diagnostics;
-use LWP::UserAgent;
 use Carp;
+use LWP::UserAgent;
 
-use version; our $VERSION = qv("0.1");
+use version; our $VERSION = qv("0.01");
 
 ### Interface
 sub new{
@@ -60,6 +59,13 @@ sub errstr{
 	return $self->{errstr};
 }
 
+sub cookie{
+	my $self = shift;
+	my $cookie = shift;
+	if ($cookie){$self->{cookie} = $cookie;}
+	return $self->{cookie};
+}
+
 ### API Calls
 sub nextuploadserver{
 	my $self = shift;
@@ -100,10 +106,9 @@ sub movefilestorealfolder {
 	my $self = shift;
 	my $realfolderid = shift;
 	
-	return unless $self->_is_init;
 	my $fileids;
 	my @fileids_arr;
-	if (ref{@_} eq 'ARRAY'){
+	if (ref @_ eq 'ARRAY'){
 		@fileids_arr = @{@_};
 		$fileids = join(',', @fileids_arr);
 	}
@@ -116,7 +121,10 @@ sub movefilestorealfolder {
 	}
 	
 	my $sub = "movefilestorealfolder_v1";
-	my $call = $self->{_url}."sub=${sub}&type=".$self->{type}."&login=".$self->{login}."&password=".$self->{password}."&files=${fileids}&realfolder=${realfolderid}";
+	my $call = $self->_default_call($sub) or return;
+	$call .= "&files=${fileids}";
+	$call .= "&realfolder=${realfolderid}";
+	
 	return $self->_get_resp($call);
 }
 
@@ -125,14 +133,16 @@ sub renamerealfolder{
 	my $realfolderid = shift;
 	my $newname = shift;
 	
-	return unless $self->_is_init;
 	if (length($newname) > 100){
 		$self->{errstr} = "Length of newname is more than 100 Chars";
 		return;
 	}
 	
 	my $sub = "renamerealfolder_v1";
-	my $call = $self->{_url}."sub=${sub}&type=".$self->{type}."&login=".$self->{login}."&password=".$self->{password}."realfolder=${realfolderid}&newname=${newname}";
+	my $call = $self->_default_call($sub) or return;
+	$call .= "realfolder=${realfolderid}";
+	$call .= "&newname=${newname}";
+	
 	return $self->_get_resp($call);
 }
 
@@ -141,7 +151,7 @@ sub deletefiles {
 	
 	my $fileids;
 	my @fileids_arr;
-	if (ref{@_} eq 'ARRAY'){
+	if (ref @_ eq 'ARRAY'){
 		@fileids_arr = @{@_};
 		$fileids = join(',', @fileids_arr);
 	}
@@ -153,10 +163,10 @@ sub deletefiles {
 		return;
 	}
 	
-	return unless $self->_is_init;
-	
 	my $sub = "deletefiles_v1";
-	my $call = $self->{_url}."sub=${sub}&type=".$self->{type}."&login=".$self->{login}."&password=".$self->{password}."&files=${fileids}";
+	my $call = $self->_default_call($sub) or return;
+	$call .= "&files=${fileids}";
+	
 	return $self->_get_resp($call);
 }
 
@@ -165,14 +175,16 @@ sub addrealfolder{
 	my $name = shift;
 	my $parent = shift or 0;
 	
-	return unless $self->_is_init;
 	if (length($name) > 100){
 		$self->{errstr} = "Length of name is greater than 100 bytes";
 		return;
 	}
 	
 	my $sub  = "addrealfolder_v1";
-	my $call = $self->{_url}."sub=${sub}&type=".$self->{type}."&login=".$self->{login}."&password=".$self->{password}."&name=${name}&parent=${parent}";
+	my $call = $self->_default_call($sub) or return;
+	$call .= "&name=${name}";
+	$call .= "&parent=${parent}";
+	
 	return $self->_get_resp($call);
 }
 
@@ -180,10 +192,10 @@ sub delrealfolder{
 	my $self = shift;
 	my $realfolder = shift;
 	
-	return unless $self->_is_init;
-	
 	my $sub = "delrealfolder_v1";
-	my $call = $self->{_url}."sub=${sub}&type=".$self->{type}."&login=".$self->{login}."&password=".$self->{password}."&realfolder=${realfolder}";
+	my $call = $self->_default_call($sub) or return;
+	$call .= "&realfolder=${realfolder}";
+	
 	return $self->_get_resp($call);
 }
 
@@ -192,10 +204,11 @@ sub moverealfolder{
 	my $realfolder = shift;
 	my $newparent = shift;
 	
-	return unless $self->_is_init;
-	
 	my $sub = "moverealfolder_v1";
-	my $call = $self->{_url}."sub=${sub}&type=".$self->{type}."&login=".$self->{login}."&password=".$self->{password}."&realfolder=${realfolder}&newparent=${newparent}";
+	my $call = $self->_default_call($sub) or return;
+	$call .= "&realfolder=${realfolder}";
+	$call .= "&newparent=${newparent}";
+	
 	return $self->_get_resp($call);
 }
 
@@ -211,10 +224,9 @@ sub listfiles{
 	$desc = $options{'desc'} if exists $options{'desc'};
 	$returnds = $options{'returnds'} if exists $options{'returnds'};
 	
-	return unless $self->_is_init;
-	
 	my $sub = "listfiles_v1";
-	my $call = $self->{_url}."sub=${sub}&type=".$self->{type}."&login=".$self->{login}."&password=".$self->{password}."&realfolder=${realfolder}";
+	my $call = $self->_default_call($sub) or return;
+	$call .= "&realfolder=${realfolder}";
 	$call .= "&filename=${filename}" if $filename;
 	$call .= "&fields=${fields}" if $fields;
 	$call .= "&order=${order}" if $order;
@@ -225,23 +237,20 @@ sub listfiles{
 	my $response = $self->_get_resp($call) or return;
 	return $response if (uc($response) eq 'NONE');
 	my @lines = split(/\n/, $response);
+	my $list;
 	if ($fields){
-		my $list;
 		my @req = split(/,/, $fields);
 		foreach my $line (@lines){
 			my ($index, $id, @resp) = split(/,/, $line);
 			my $len = @req; my $i = 0;
-			while($i < $len){$list->{"$id"}->{"$req[$i]"} = $resp[$i]; $i++;}
-			$list->{"$id"}->{"index"}=$index;
+			while($i < $len){$list->{"$index"}->{"$req[$i]"} = $resp[$i]; $i++;}
+			$list->{"$index"}->{"fileid"}=$id;
 		}
-		return $list;
 	}
-	my @fileids;
-	foreach my $line (@lines){
-		my (undef, $id) = split(/,/, $line);
-		push(@fileids, $id);
+	else{
+		foreach my $line (@lines){my ($index,$id) = split(/,/, $line); $list->{"$index"}->{"fileid"}=$id;}
 	}
-	return @fileids;
+	return $list;
 }
 
 sub listrealfolders{
@@ -250,10 +259,8 @@ sub listrealfolders{
 	my %options; %options = %{_read_opts(@_)} if @_;
 	my $returnds; $returnds = $options{'returnds'} if exists $options{'returnds'};
 	
-	return unless $self->_is_init;
-	
 	my $sub = "listrealfolders_v1";
-	my $call = $self->{_url}."sub=${sub}&type=".$self->{type}."&login=".$self->{login}."&password=".$self->{password};
+	my $call = $self->_default_call($sub) or return;
 	return $self->_get_resp($call) unless $returnds;
 	
 	my $response = $self->_get_resp($call) or return;
@@ -277,13 +284,72 @@ sub getaccountdetails{
 	$withcookie = $options{'withcookie'} if exists $options{'withrefstring'};
 	$returnds = $options{'returnds'} if exists $options{'returnds'};
 	
-	return unless $self->_is_init;
-	
 	my $sub = "getaccountdetails_v1";
-	my $call = $self->{_url}."sub=${sub}&type=".$self->{type}."&login=".$self->{login}."&password=".$self->{password};
+	my $call = $self->_default_call($sub) or return;
 	$call .= "&withrefstring=1" if $withrefstring;
 	$call .= "&withcookie=1" if $withcookie;
 	return $self->_get_resp($call) unless $returnds;
+	
+	my $response = $self->_get_resp($call) or return;
+	my @lines = split(/\n/, $response);
+	my $accnt;
+	foreach my $line (@lines){
+		my ($key,$value) = split(/=/, $line);
+		$accnt->{"$key"} = $value;
+	}
+	return $accnt;
+}
+
+sub setaccountdetails{
+	my $self = shift;
+	
+	my %options; %options = %{_read_opts(@_)} if @_;
+	my ($newpassword,$email,$username,$mirror, $mirror2, $mirror3, $directstart, $jsconfig, $plustrafficmode) = 0;
+	croak "email not provided" unless exists $options{'email'};
+	$email = $options{'email'};
+	$newpassword = $options{'newpassword'} if exists $options{'newpassword'};
+	$username = $options{'username'} if exists $options{'username'};
+	$mirror = $options{'mirror'} if exists $options{'mirror'};
+	$mirror2 = $options{'mirror2'} if exists $options{'mirror2'};
+	$mirror3 = $options{'mirror3'} if exists $options{'mirror3'};
+	$directstart = $options{'directstart'} if exists $options{'directstart'};
+	$jsconfig = $options{'jsconfig'} if exists $options{'jsconfig'};
+	$plustrafficmode = $options{'plustrafficmode'} if exists $options{'plustrafficmode'};	
+	
+	my $sub = "setaccountdetails_v1";
+	my $call = $self->_default_call($sub) or return;
+	$call .= "&email=${email}";
+	$call .= "&newpassword=${newpassword}" if $newpassword;
+	$call .= "&username=${username}" if $username;
+	$call .= "&mirror=${mirror}" if $mirror;
+	$call .= "&mirror2=${mirror2}" if $mirror2;
+	$call .= "&mirror3=${mirror3}" if $mirror3;
+	$call .= "&directstart=${directstart}" if $directstart;
+	$call .= "&jsconfig=${jsconfig}" if $jsconfig;
+	$call .= "&plustrafficmode=${plustrafficmode}" if exists $options{'plustrafficmode'};
+	
+	return $self->_get_resp($call);
+}
+
+sub enablersantihack{
+	my $self = shift;
+	
+	my $sub = "enablersantihack_v1";
+	my $call = $self->_default_call($sub) or return;
+	return $self->_get_resp($call);
+}
+
+sub disablersantihack{
+	my $self = shift;
+	
+	my %options; %options=%{_read_opts(@_)} if @_;
+	my $unlockcode = $options{'unlockcode'} or croak "Unlock code not provided";
+	
+	my $sub = "disablersantihack_v1";
+	my $call = $self->_default_call($sub) or return;
+	$call .= "&unlockcode=${unlockcode}";
+	
+	return $self->_get_resp($call);
 }
 
 ### Internal Utilities
@@ -296,7 +362,7 @@ sub _get_resp{
 	$ua->proxy('http', $self->{proxy}) if $self->{proxy};
 	
 	my $response = $ua->get($call);
-	$self->{errstr} = $response->status_line and return unless $response->is_success;
+	unless ($response->is_success){	$self->{errstr} = $response->status_line;return;}
 	my $resp_str = ${$response->content_ref};
 	return $resp_str if $self->_response_is_good($resp_str);
 	return;
@@ -305,13 +371,13 @@ sub _get_resp{
 sub _response_is_good{
 	my $self = shift;
 	my $str = shift;
-	$self->{errstr} = $str and return if ($str =~ m/^ERROR/g);
+	if ($str =~ /^ERROR/){$self->{errstr} = $str;return;}
 	return 1;
 }
 
 sub _valid_type{
 	my $type = shift or return;
-	my %valid = ("col" => 1, "perm"=>1);
+	my %valid = ("col" => 1, "prem"=>1);
 	return $valid{$type} if $valid{$type};
 	return;
 }
@@ -325,7 +391,23 @@ sub _is_init{
 
 sub _read_opts{
 	my %options;
-	if(ref{@_} eq 'HASH'){%options = %{@_};}else{(%options) = @_;}
+	if(ref @_ eq 'HASH'){%options = %{@_};}else{(%options) = @_;}
 	return \%options;
 }
+
+sub _default_call{
+	my $self = shift;
+	my $sub = shift;
+	
+	return unless $self->_is_init;
+	my $call = $self->{_url};
+	$call .= "sub=${sub}";
+	$call .= "&type=".$self->{type};
+	$call .= "&login=".$self->{login};
+	$call .= "&password=".$self->{password};
+	$call .= "&cookie=".$self->{cookie} if $self->{cookie};
+	
+	return $call;
+}
+
 1;
